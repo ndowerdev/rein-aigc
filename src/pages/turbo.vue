42a22b238293fcf6ef87dac66e60d107ab4d5ca7
Rendrian Arma <hi@rendrian.dev>
2023-03-08 21:15:46 +0700

    change divider name

--- src/pages/turbo.vue
+++ src/pages/turbo.vue
@@ -196,7 +196,7 @@ watch(
               </div>
             </div>
 
-            <Divider title="Action" />
+            <Divider title="Actions" />
             <div class="flex flex-wrap -mx-3">
               <div class=" w-1/2 px-3">
                 <button
