a93dd341b0302b92e54eb2c61fec7d90ae29f105
Rendrian Arma <hi@rendrian.dev>
2023-03-08 22:52:19 +0700

    balek lagi ke awal wkwkwk

---
+++ dist/assets/dashboard-9c1d9807.js
@@ -0,0 +1,27 @@
+import{f as w,r as v,o as a,c as t,b as e,F as c,g as d,a as r,w as u,h as _,t as h,e as m,i as b,j as i}from"./app-64104e41.js";const p=[{name:"ChatGPT Turbo",routeName:"openai-turbo"},{name:"Davinci 003",routeName:"reinsaigc-index"}],f={},x={class:"navbar bg-neutral"},N={class:"navbar-start"},L={class:"dropdown"},k=e("label",{tabindex:"0",class:"btn btn-ghost lg:hidden"},[e("svg",{xmlns:"http://www.w3.org/2000/svg",class:"h-5 w-5",fill:"none",viewBox:"0 0 24 24",stroke:"currentColor"},[e("path",{"stroke-linecap":"round","stroke-linejoin":"round","stroke-width":"2",d:"M4 6h16M4 12h8m-8 6h16"})])],-1),M={tabindex:"0",class:"menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"},T=e("a",{class:"btn btn-ghost normal-case text-xl"},"REIN-AI",-1),B={class:"navbar-center hidden lg:flex"},V={class:"menu menu-horizontal px-1 gap-2"},y=e("div",{class:"navbar-end"},[e("a",{class:"btn btn-ghost font-bold bg-base-300",href:"https://saweria.co/ndower",target:`_blank\r
+      `},"DONATE")],-1);function C(s,g){const l=v("RouterLink");return a(),t("div",x,[e("div",N,[e("div",L,[k,e("ul",M,[(a(!0),t(c,null,d("NavMenus"in s?s.NavMenus:m(p),(n,o)=>(a(),t("li",{key:o},[r(l,{to:{name:n.routeName}},{default:u(()=>[_(h(n.name),1)]),_:2},1032,["to"])]))),128)),b(` <li tabindex="0">\r
+            <a class="justify-between">\r
+              Parent\r
+              <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">\r
+                <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />\r
+              </svg>\r
+            </a>\r
+            <ul class="p-2">\r
+              <li><a>Turbo</a></li>\r
+              <li><a></a></li>\r
+            </ul>\r
+          </li>\r
+          <li><a>Item 3</a></li> `)])]),T]),e("div",B,[e("ul",V,[(a(!0),t(c,null,d("NavMenus"in s?s.NavMenus:m(p),(n,o)=>(a(),t("li",{key:o,class:"font-bold bg-base-300"},[r(l,{to:{name:n.routeName}},{default:u(()=>[_(h(n.name),1)]),_:2},1032,["to"])]))),128)),b(` <li><a>Item 1</a></li>\r
+        <li tabindex="0">\r
+          <a>\r
+            Parent\r
+            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">\r
+              <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />\r
+            </svg>\r
+          </a>\r
+          <ul class="p-2">\r
+            <li><a>Submenu 1</a></li>\r
+            <li><a>Submenu 2</a></li>\r
+          </ul>\r
+        </li>\r
+        <li><a>Item 3</a></li> `)])]),y])}const D=w(f,[["render",C],["__file","D:/laragon/www/openai-prompt-tester/src/components/TheNavbar.vue"]]),I={class:""},R={class:"w-full xl:w-2/3 m-auto h-screen"},$=e("div",null,null,-1),j={__name:"dashboard",setup(s){return i(!0),i(!1),i(!1),(g,l)=>{const n=D,o=v("RouterView");return a(),t("main",I,[e("div",R,[r(n),$,r(o)])])}}},P=w(j,[["__file","D:/laragon/www/openai-prompt-tester/src/layouts/dashboard.vue"]]);export{P as default};
